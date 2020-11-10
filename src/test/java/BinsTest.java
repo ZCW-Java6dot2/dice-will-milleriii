527b448b71f56609af67648716a0167cff8e6197
will <will@zipcodes-MBP-8.fios-router.home>
2020-11-09 22:21:13 -0500

    still working on tests and methods

---
+++ src/test/java/BinsTest.java
@@ -0,0 +1,18 @@
+
+import org.junit.jupiter.api.Test;
+public class BinsTest {
+
+    @Test
+    public void binSetUp() {
+        Bins bin = new Bins(2,12);
+        Integer actualFirst = 2;
+        Integer actualLast = 12;
+        Integer expectedFirst = bin.getFirst();
+        Integer expectedLast = bin.getLast();
+        Integer expected = 0;
+
+
+
+    }
+
+}
